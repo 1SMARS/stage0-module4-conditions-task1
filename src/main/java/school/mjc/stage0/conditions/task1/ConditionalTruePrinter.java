c428554992c806601eee92df0019808edbf0c6b6
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-10-10 21:28:39 +0600

    Update ConditionalTruePrinter.java

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int c = 1;
+
+        if (c == 1) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
